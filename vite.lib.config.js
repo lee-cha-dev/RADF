5ad01ff88f7d1cc83c6b11dc7d5dbee9ef585773
lee-cha-dev <130196023+lee-cha-dev@users.noreply.github.com>
2026-01-24 19:50:59 -0600

    Fix library style generation for consumers

--- vite.lib.config.js
+++ vite.lib.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    cssCodeSplit: false,
     lib: {
       entry: 'src/framework/index.js',
       name: 'RADF',
